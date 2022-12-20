92bffa057ee2ec6c0bdcbc869bbc6bf7fa36862f
yung-coder <chandel.sanjeev5@gmail.com>
2022-12-20 23:48:24 +0530

    Videos comp

--- src/components/ChannelCard.tsx
+++ src/components/ChannelCard.tsx
@@ -4,7 +4,22 @@ type ChannelCardProps = {
   items: any;
 };
 
-const ChannelCard: React.FC<ChannelCardProps> = ({items}) => {
-  return <div>Have a good cahnndel</div>;
+const ChannelCard: React.FC<ChannelCardProps> = ({ items }) => {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center p-3 space-y-2">
+        <div>
+          <img
+            src={items.snippet.thumbnails.high.url}
+            alt=""
+            className="h-40 w-40 rounded-full"
+          />
+        </div>
+        <div>
+          <h2 className="text-white font-bold">{items.snippet.channelTitle}</h2>
+        </div>
+      </div>
+    </>
+  );
 };
 export default ChannelCard;
--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -11,9 +11,9 @@ const Videos: React.FC<VideosProps> = ({ videos }) => {
     <div className="flex flex-wrap border border-blue-800 space-x-3 justify-around h-[733px]  p-4 space-y-2 overflow-auto">
       {videos?.map((items, i) => {
         return (
-          <div key={i} className=''>
-            {items.id.videoId && <VideoCard items={items}/>}
-            {items.id.channelId && <ChannelCard  items={items}/>}
+          <div key={i} className="">
+            {items.id.videoId && <VideoCard items={items} />}
+            {items.id.channelId && <ChannelCard items={items} />}
           </div>
         );
       })}
